f6ebfe4ecc8d34ed42a6ec6bd4be2654940976a0
Aritra Majumdar <aritram21@vt.edu>
2023-05-08 17:29:31 -0400

    Cross Validation

--- Cross Validation/tiger.m
+++
@@ -1,9 +0,0 @@
-clc, clear all; close all;
-ds = datastore(fullfile('C:\Users\user\Desktop\Data\atrw_pose_train\train'),...
-'IncludeSubfolders', true,'Type', 'image');
-fname = 'C:\Users\user\Desktop\Data\atrw_anno_pose_train\keypoint_train.json'; 
-fid = fopen(fname); 
-raw = fread(fid,inf); 
-str = char(raw'); 
-fclose(fid); 
-val = jsondecode(str);
